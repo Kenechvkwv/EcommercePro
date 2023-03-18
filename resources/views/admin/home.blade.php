8a859e3c7e1ed8cec01e995d024676f2a76f09ff
kenechvkwv <ubesiekene@gmail.com>
2023-03-18 03:43:10 +0100

    updates

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,23 +1,24 @@
 <x-app-layout>
-    <!DOCTYPE html>
-    <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-        <style>
-            h1 {
-                font-weight: bold;
-                font-size: 30px;
-            }
-        </style>
-    </head>
-
-    <body>
-        <h1>Admin</h1>
-    </body>
-
-    </html>
 </x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        h1 {
+            font-weight: bold;
+            font-size: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Admin</h1>
+</body>
+
+</html>
